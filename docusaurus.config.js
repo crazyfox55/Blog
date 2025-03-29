7cbed6f4249641637ce11d96a2ba9d854c9bd6f4
David McDermott <mcdermott.j.david@gmail.com>
2025-03-29 14:11:56 -0400

    Fix the edit links with the right trunk

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/crazyfox55/Blog',
+          editUrl: 'https://github.com/crazyfox55/Blog/blob/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/crazyfox55/Blog',
+          editUrl: 'https://github.com/crazyfox55/Blog/blob/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
